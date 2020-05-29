89e209ac23f5ce1f9c48649527fbce13bd44336b
xiangrong.liu_刘祥荣 <xiangrong.liu@wehotelglobal.com>
2020-05-29 19:18:00 +0800

    feat: 020 done

--- 020.有效的括号.js
+++ 020.有效的括号.js
@@ -3,24 +3,28 @@
  * @author: xiangrong.liu
  * @Date: 2020-05-29 16:27:40
  * @LastEditors: xiangrong.liu
- * @LastEditTime: 2020-05-29 17:59:25
+ * @LastEditTime: 2020-05-29 18:54:45
  */ 
 /*
 1. 阿里健康第一面的算法题，当时没有复习算法，所以就跪了，当我看到了答案之后，心很痛，原来很简单，痛定思痛，怒刷leetcode
 2. 对时间复杂度的评估不熟悉（以为遍历一次都会导致时间复杂度高）
 3. 栈，先进先出
+4. 测试用例不全，有个别逻辑缺失，即缺少考虑边界情况
 */
 
 
 // let str = 'abc(123)'
-let str = '[(123)]'
+// let str = '[(123)]'
 // let str = '{123)'
 // let str = '()[]'
+// let str = ']'
+let str = '(])'
 
-function isClosure(str) {
-    if (Object.prototype.toString.call(str) !== '[object String]') {
-        throw new Error('请传入字符串')
+var isValid = function(s) {
+    if (!s) {
+        return true
     }
+    let valid = false;
     let left = ['{', '[', '(']
     let right = ['}', ']', ')']
     let stack = [];
@@ -29,18 +33,21 @@ function isClosure(str) {
         ']': '[',
         ')': '('
     }
-    for (let i = 0; i < str.length; i++) {
-        if (left.includes(str[i])) {
-            stack.push(str[i])
-        }
-        console.log(stack)
-        if (right.includes(str[i])) {
-            if (stack[stack.length -1] === match[str[i]]) {
+    for (let i = 0; i < s.length; i++) {
+        if (left.includes(s[i])) {
+            stack.push(s[i])
+        } else if (right.includes(s[i])) {
+            if (stack[stack.length -1] === match[s[i]]) {
                 stack.pop()
+                if (!stack.length) {
+                    valid = true
+                }
+            } else {
+                return false
             }
         }
     }
-    return !stack.length
-}
+    return valid
+};
 
-console.log(isClosure(str))
+console.log(isValid(str))
