eba1dd690badb35b5da3c6f16f5183bc58a597db
liuxiangrong <liuxiangrong@banggood.com>
2020-05-30 10:48:58 +0800

    feat: 021 think done

--- 021.合并两个有序链表.js
+++ 021.合并两个有序链表.js
@@ -6,48 +6,60 @@
  * @LastEditTime: 2020-05-29 18:54:45
  */ 
 /*
-1. 阿里健康第一面的算法题，当时没有复习算法，所以就跪了，当我看到了答案之后，心很痛，原来很简单，痛定思痛，怒刷leetcode
-2. 对时间复杂度的评估不熟悉（以为遍历一次都会导致时间复杂度高）
-3. 栈，先进先出
-4. 测试用例不全，有个别逻辑缺失，即缺少考虑边界情况
+1. 没有审题，一个升序链表，就不需要考虑重新排序了，又想复杂
+2. 对链表的操作不熟悉，查找资料得出，链表的实现，有两个类，一个是ListNode，一个是LinkedList。这里只需要用到ListNodel.https://juejin.im/entry/59cb70995188256aa423b680
 */
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 
-// let str = 'abc(123)'
-// let str = '[(123)]'
-// let str = '{123)'
-// let str = '()[]'
-// let str = ']'
-let str = '(])'
-
-var isValid = function(s) {
-    if (!s) {
-        return true
+// 暴力解法 递归
+// 时间复杂度：O(m+n)
+// 空间复杂度：O(m+n) 考虑递归最深的时候要用到所有的空间
+var mergeTwoLists = function(l1, l2) {
+    if (l1 === null) {
+        return l2
+    } else if (l2 === null) {
+        return l1
+    } else if ( l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
     }
-    let valid = false;
-    let left = ['{', '[', '(']
-    let right = ['}', ']', ')']
-    let stack = [];
-    let match = {
-        '}': '{',
-        ']': '[',
-        ')': '('
-    }
-    for (let i = 0; i < s.length; i++) {
-        if (left.includes(s[i])) {
-            stack.push(s[i])
-        } else if (right.includes(s[i])) {
-            if (stack[stack.length -1] === match[s[i]]) {
-                stack.pop()
-                if (!stack.length) {
-                    valid = true
-                }
-            } else {
-                return false
-            }
-        }
-    }
-    return valid
 };
 
-console.log(isValid(str))
+// 迭代
+// 时间复杂度：O(m+n)
+// 空间复杂度：O(1) 只需要常数的空间存放若干变量
+var mergeTwoLists = function(l1, l2) {
+    const prehead = new ListNode(-1);
+
+    let prev = prehead;
+    while (l1 != null && l2 != null) {
+        if (l1.val <= l2.val) {
+            prev.next = l1;
+            l1 = l1.next;
+        } else {
+            prev.next = l2;
+            l2 = l2.next;
+        }
+        prev = prev.next;
+    }
+
+    // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+    prev.next = l1 === null ? l2 : l1;
+
+    return prehead.next;
+};
